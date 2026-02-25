d3701610b51851dce3aad1c4b709b4d3eacf3ba0
Ayodhya Rode <ayodhyarode168@gmail.com>
2026-02-25 06:50:12 +0530

    Database Connection

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./src/app.js";
+import connectToDB from "./src/DB/db.js";
+
+connectToDB();
+
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is listening`)
