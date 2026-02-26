a9047eb9f4df9552dc65cd3e48a1ad1dfbe0b756
Ayodhya Rode <ayodhyarode168@gmail.com>
2026-02-26 13:14:15 +0530

    cors setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import adminRegisterRoute from "./routes/authAdminRoute.js";
-
+import cors from "cors"
 
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 app.use(cookieParser())
 
 app.use("/api/admin", adminRegisterRoute)
